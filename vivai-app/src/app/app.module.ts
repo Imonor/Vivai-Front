9a150036059b6bbba3daa4388a3317f6ee7ea8af
Martin <martin.germain@insa-lyon.fr>
2020-04-16 13:15:57 +0200

    Update dashboard
    Card Plant a changer En une seul card et pas une liste ;)

--- vivai-app/src/app/app.module.ts
+++ vivai-app/src/app/app.module.ts
@@ -7,6 +7,16 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PlantCardComponent} from './dashboard/dashboard/plant-card/plant-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 
 @NgModule({
@@ -22,7 +32,15 @@ import { PlantCardComponent} from './dashboard/dashboard/plant-card/plant-card.c
     RouterModule.forRoot([
       { path: '', component: LandingPageComponent},
       { path: 'dashboard', component: DashboardComponent },
-    ])
+    ]),
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
