0d59c1411a913e9f38f065d3e0b107b0fc44b525
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2020-04-16 10:41:03 +0200

    add plant card component

---
+++ vivai-app/src/app/dashboard/dashboard/plant-card/plant-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plant-card',
+  templateUrl: './plant-card.component.html',
+  styleUrls: ['./plant-card.component.scss']
+})
+export class PlantCardComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
