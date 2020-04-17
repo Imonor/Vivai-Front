9b4b5fd4a61a9f0312663fa18fbea81cc1556ee1
Martin <martin.germain@insa-lyon.fr>
2020-04-17 14:01:44 +0200

    init angular

---
+++ vivai-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vivai-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'vivai-app';
+}
