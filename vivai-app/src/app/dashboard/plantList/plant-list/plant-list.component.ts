616ff0c26114c5ebf15e0cb852e3054b2b0aeb20
Martin <martin.germain@insa-lyon.fr>
2020-04-17 16:34:23 +0200

    Add Patch Plant card + Theme

--- vivai-app/src/app/dashboard/plantList/plant-list/plant-list.component.ts
+++ vivai-app/src/app/dashboard/plantList/plant-list/plant-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plant-list.component.scss']
 })
 export class PlantListComponent implements OnInit {
-
+  coucouTab = [1,2,3,4,5,6];
   constructor() { }
 
   ngOnInit() {
