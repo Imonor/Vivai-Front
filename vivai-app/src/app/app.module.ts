1433122c1a062b57fe920a1eedb4fd82e49d9e6a
Martin <martin.germain@insa-lyon.fr>
2020-04-18 11:32:47 +0200

    Material Module
    Module thzt import all the material comp easily

--- vivai-app/src/app/app.module.ts
+++ vivai-app/src/app/app.module.ts
@@ -9,14 +9,7 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { PlantListComponent } from './dashboard/plantList/plant-list/plant-list.component';
 import { PlantCardComponent } from './dashboard/plantCard/plant-card/plant-card.component';
 import { InfoCardComponent } from './dashboard/infoCard/info-card/info-card.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
+import { MaterialModule } from './material/material.module';
 
 
 @NgModule({
@@ -32,14 +25,7 @@ import {MatCardModule} from '@angular/material/card';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonToggleModule,
-    MatListModule,
-    MatToolbarModule,
-    MatCardModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
