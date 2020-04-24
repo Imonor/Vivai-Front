804e94d028019614b593f58b8b7a2f7d11be1f57
Martin <martin.germain@insa-lyon.fr>
2020-04-24 11:01:03 +0200

    Card Responsive + animation

--- vivai-app/src/app/dashboard/plantList/plant-list/plant-list.component.ts
+++ vivai-app/src/app/dashboard/plantList/plant-list/plant-list.component.ts
@@ -8,17 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class PlantListComponent implements OnInit {
   listPlante = [
     {
-      nom: "Basilic",
+      nom: "Marco",
+      type: "Basilic",
       image: "https://media.ooreka.fr/public/image/plant/16/varietyImage/10l1k2476if4404sgowwc8kow-source-9229039.jpg",
       piece: "Jardin",
     },
     {
-      nom: "Olivier",
+      nom: "Tomy",
+      type: "Olivier",
       image: "https://media.ooreka.fr/public/image/plant/4/furtherImage/34265a2z3esks4wkcgsk8s04w-source-8883047.jpg",
       piece: "Jardin",
     },
     {
-      nom: "Campanule",
+      nom: "Jane",
+      type: "Campanule",
+      image: "https://media.ooreka.fr/public/image/plant/8/furtherImage/3mq1gdyj9iecwgc4kksw4owks-source-9131594.jpg",
+      piece: "Jardin",
+    },
+    {
+      nom: "Julia",
+      type: "Campanule",
       image: "https://media.ooreka.fr/public/image/plant/8/furtherImage/3mq1gdyj9iecwgc4kksw4owks-source-9131594.jpg",
       piece: "Jardin",
     }
