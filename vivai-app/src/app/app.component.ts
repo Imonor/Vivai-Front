ca73a3854e65dc0237984dcaf77e9ed08e0e6402
Martin <martin.germain@insa-lyon.fr>
2020-04-27 11:36:21 +0200

    Fix btn lila LougedOut + Ignore Amplify

--- vivai-app/src/app/app.component.ts
+++ vivai-app/src/app/app.component.ts
@@ -4,12 +4,13 @@ import {
   EventEmitter,
   Output,
   OnChanges,
-  OnInit} from '@angular/core';
+  OnInit
+} from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatSidenav, MatSnackBar, MatDialog, MatDialogConfig } from '@angular/material';
-import {Location} from '@angular/common';
 import { IosInstallComponent } from './ios-install/ios-install.component';
 import { LilaComponent } from './lila/lila.component';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -36,7 +37,7 @@ export class AppComponent implements OnInit {
   private _mobileQueryListener: () => void;
   @Output() toggleSideNav = new EventEmitter();
 
-  constructor( changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private toast: MatSnackBar, private dialog: MatDialog ) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private toast: MatSnackBar, private dialog: MatDialog, public router: Router) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -46,7 +47,7 @@ export class AppComponent implements OnInit {
     // Detects if device is on iOS
     const isIos = () => {
       const userAgent = window.navigator.userAgent.toLowerCase();
-      return /iphone|ipad|ipod/.test( userAgent );
+      return /iphone|ipad|ipod/.test(userAgent);
     }
     // Detects if device is in standalone mode
     const isInStandaloneMode = () => ('standalone' in (window as any).navigator) && ((window as any).navigator.standalone);
@@ -68,9 +69,10 @@ export class AppComponent implements OnInit {
   }
 
   displayButtonLila() {
-    if (window.innerWidth < 1000) {
+    if (window.innerWidth < 1000 && (this.router.url !== "/")) {
       return true;
     } else return false;
+
   }
 
   openDialog() {
