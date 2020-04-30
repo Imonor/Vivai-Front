3ccc4a0aa007739e3a28b5b71916217ab717296f
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2020-04-30 13:48:26 +0200

    Refonte du panneau "fait aujourd'hui"

--- vivai-app/src/app/dashboard/plant-page/plant-page.component.ts
+++ vivai-app/src/app/dashboard/plant-page/plant-page.component.ts
@@ -16,9 +16,16 @@ export class PlantPageComponent implements OnInit {
       iconRegistry.addSvgIcon(
         'water',
         sanitizer.bypassSecurityTrustResourceUrl('assets/svg/water.svg'));
-        // Ajouter kes autres icons
-      
-     }
+      iconRegistry.addSvgIcon(
+        'prune',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/svg/prune.svg'));
+      iconRegistry.addSvgIcon(
+        'harvest',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/svg/harvest.svg'));
+      iconRegistry.addSvgIcon(
+        'repoting',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/svg/repoting.svg'));
+    }
 
   ngOnInit() {
     this.catchPlantFroHistory();
