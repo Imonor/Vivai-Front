0dceecfa2a38b946cc8b6e8f4ef0705e188c69c7
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2020-04-30 16:27:46 +0200

    Ajout du nombre de tâches validées

--- vivai-app/src/app/dashboard/plant-page/plant-page.component.ts
+++ vivai-app/src/app/dashboard/plant-page/plant-page.component.ts
@@ -4,6 +4,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
 import { LoaderService } from 'src/app/loader/loader.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'vivai-plant-page',
@@ -16,9 +17,11 @@ export class PlantPageComponent implements OnInit {
   private _mobileQueryListener: () => void;
 
   curentPlant: null;
+  public reportingForm: FormGroup;
+  taskNumber: 0;
 
   constructor(public _loading: LoaderService,iconRegistry: MatIconRegistry,
-    sanitizer: DomSanitizer, media: MediaMatcher, changeDetectorRef: ChangeDetectorRef,private router: Router) {
+    sanitizer: DomSanitizer, media: MediaMatcher, changeDetectorRef: ChangeDetectorRef,private router: Router, private fb: FormBuilder) {
       this.mobileQuery = media.matchMedia('(max-width: 600px)');
       this._mobileQueryListener = () => changeDetectorRef.detectChanges();
       this.mobileQuery.addListener(this._mobileQueryListener);
@@ -39,6 +42,13 @@ export class PlantPageComponent implements OnInit {
 
   ngOnInit() {
     this.catchPlantFroHistory();
+    this.reportingForm = this.fb.group({
+      water: [false],
+      prune: [false],
+      repoting: [false],
+      harvest: [false]
+    });
+    this.taskNumber = 0;
   }
 
 
@@ -50,4 +60,23 @@ export class PlantPageComponent implements OnInit {
     
     console.log(this.curentPlant);
   }
+
+  checkTaskNumber() {
+    this.taskNumber = 0;
+    setTimeout(() => {
+    if (this.reportingForm.get('water').value) {
+      this.taskNumber++;
+    }
+    if (this.reportingForm.get('prune').value) {
+      this.taskNumber++;
+    }
+    if (this.reportingForm.get('repoting').value) {
+      this.taskNumber++;
+    }
+    if (this.reportingForm.get('harvest').value) {
+      this.taskNumber++;
+    }
+  });
+}
+
 }
